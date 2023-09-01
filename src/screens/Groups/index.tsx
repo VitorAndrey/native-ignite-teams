d4ad71d2759c16ef7d4c67497c63f6121ffa5adc
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-08-31 22:08:39 -0300

    first commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Title } from "./styles";
+
+export function Groups() {
+  return (
+    <Container>
+      <Title>NOIBGA</Title>
+    </Container>
+  );
+}
